fcc19ab491e2c7aa8af56a8b74efd9f2c627a404
Andrey <am.xcvi@gmail.com>
2024-06-06 13:46:11 +0300

    fix tailwind config

--- src/pages/error-page/ui/error-page.tsx
+++ src/pages/error-page/ui/error-page.tsx
@@ -1,5 +1,5 @@
 const ErrorPage = () => {
-    return <div></div>;
+    return <div>Error page</div>;
 };
 
 export default ErrorPage;
--- src/pages/home-page/ui/home-page.tsx
+++ src/pages/home-page/ui/home-page.tsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-    return <></>;
+    return <section className="h-dvh">Home page</section>;
 };
 
 export default HomePage;
