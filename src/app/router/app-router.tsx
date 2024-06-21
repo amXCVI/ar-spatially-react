5e9d3ff29559c466089c3247f9daacca45c2a0f7
Andrey <am.xcvi@gmail.com>
2024-06-21 10:56:36 +0300

    add base tag

--- src/app/router/app-router.tsx
+++ src/app/router/app-router.tsx
@@ -1,3 +1,4 @@
+import MapPage from "@/pages/map-page";
 import { routes } from "@/shared/config";
 import { BaseLayout } from "@/shared/ui/layouts";
 import { lazy } from "react";
@@ -5,7 +6,6 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const ErrorPage = lazy(() => import("@/pages/error-page"));
 const HomePage = lazy(() => import("@/pages/home-page"));
-import MapPage from "@/pages/map-page";
 
 const AppRouter = () => {
     const routers = createBrowserRouter([
@@ -16,6 +16,7 @@ const AppRouter = () => {
                 { index: true, element: <HomePage /> },
 
                 { path: routes.map, element: <MapPage /> },
+
                 { path: routes.undefined, element: <ErrorPage /> },
             ],
         },
