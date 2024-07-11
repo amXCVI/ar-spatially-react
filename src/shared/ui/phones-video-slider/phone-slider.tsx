3e2d0fa58715ef5d14e97dd44f6c9118b31bf364
Andrey <am.xcvi@gmail.com>
2024-07-11 16:25:58 +0300

    fix phone slider

--- src/shared/ui/phones-video-slider/phone-slider.tsx
+++ src/shared/ui/phones-video-slider/phone-slider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 import leftArrow from "/images/product/left-arrow.svg";
 
@@ -10,10 +10,30 @@ interface PhoneSliderProps {
 }
 const PhoneSlider = ({ bgElement, videos, className, height }: PhoneSliderProps) => {
     const [selectedSlideId, setSelectedSlideId] = useState<number>(0);
+    const [phoneSize, setPhoneSize] = useState<{ width: number; height: number }>({ width: 0, height: 0 });
+
+    useEffect(() => {
+        const phone = document.getElementById("phone-border-img");
+        const videoContainer = document.getElementById("video-rect-container");
+
+        console.log(phone);
+        if (phone && videoContainer) {
+            const clientRect = phone.getBoundingClientRect();
+            setPhoneSize({ width: clientRect.width, height: clientRect.height });
+
+            console.log({ width: clientRect.width, height: clientRect.height });
+
+            videoContainer.style.width = `calc(${clientRect.width}px - 1rem)`;
+            videoContainer.style.height = `calc(${clientRect.height}px - 1rem)`;
+        }
+
+        // return () => {
+        //     phoneSize.current = { width: 0, height: 0 };
+        // };
+    }, []);
 
     const handleNextSlide = () => {
         setSelectedSlideId((e) => {
-            console.log(e);
             if (e === videos.length - 1) {
                 return 0;
             } else {
@@ -34,10 +54,40 @@ const PhoneSlider = ({ bgElement, videos, className, height }: PhoneSliderProps)
 
     return (
         <div className={className}>
-            <div className="relative">
+            <div className="relative flex justify-center items-center h-full" style={{ maxHeight: height }}>
                 {bgElement}
 
+                {/* <Phone className={`z-10 w-full`} style={{ height: height, maxHeight: height }} /> */}
+
+                <img
+                    src="/images/product/phone.svg"
+                    className={`z-10 max-h-[inherit] w-fit`}
+                    id="phone-border-img"
+                    //  style={{ maxHeight: height }}
+                />
+
                 <div
+                    id="video-rect-container"
+                    className="absolute rounded-[4vh] overflow-hidden w-60 h-full"
+                    // style={{ height: `calc(${200}px-6rem)` }}
+                    // style={{
+                    //     height: `${phoneSize.height}px)`,
+                    //     width: `${phoneSize.width}px)`,
+                    // }}
+                >
+                    <video
+                        className="h-full w-full object-cover"
+                        src={videos[selectedSlideId]}
+                        playsInline
+                        autoPlay
+                        muted
+                        onEnded={() => {
+                            handleNextSlide();
+                        }}
+                    />
+                </div>
+
+                {/* <div
                     className={`bg-[url(/images/product/phone.svg)] bg-cover bg-no-repeat bg-center overflow-hidden
                         mx-auto p-3
                         aspect-[165/343]
@@ -46,21 +96,10 @@ const PhoneSlider = ({ bgElement, videos, className, height }: PhoneSliderProps)
                         `}
                     style={{ maxHeight: height }}
                 >
-                    <div className="h-full rounded-[4vh] overflow-hidden">
-                        <video
-                            className="h-full w-full object-cover"
-                            src={videos[selectedSlideId]}
-                            playsInline
-                            autoPlay
-                            muted
-                            onEnded={() => {
-                                handleNextSlide();
-                            }}
-                        />
-                    </div>
-                </div>
+                    
+                </div> */}
             </div>
-            <div className="flex justify-around items-center mt-8 mx-auto max-w-min">
+            <div className="relative flex justify-around items-center mt-8 mx-auto max-w-min z-10">
                 <div className="cursor-pointer w-10" onClick={handlePrevSlide}>
                     <img src={leftArrow} />
                 </div>
