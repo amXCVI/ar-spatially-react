8eef192077d8f72169659370e933b178c110d471
Andrey <am.xcvi@gmail.com>
2024-07-19 12:54:09 +0300

    add bottom sheet

--- src/features/nft-item/ui/nft-item.tsx
+++ src/features/nft-item/ui/nft-item.tsx
@@ -1,5 +1,6 @@
 import { MarkerInterface } from "@ar-kit/lib";
 import { useOutsideClick } from "@ar-kit/shared/hooks";
+import { Sheet } from "react-modal-sheet";
 
 import { TextP, Title } from "@/shared/ui/text-components";
 
@@ -17,42 +18,51 @@ const NftItem = ({ selectedMarker, onCloseViewer }: NftItemInterface) => {
     });
 
     return (
-        <div
-            className={`fixed right-0 bottom-0 left-0 ${selectedMarker ? "h-dvh" : "h-0"} duration-1000
-                        flex justify-center items-center bg-white30
-                        ${selectedMarker ? "p-5 md:p-10 lg:p-20 pt-40 md:pt-10" : "p-0"} z-30`}
-        >
-            {!!selectedMarker && (
-                <div
-                    className="h-full max-w-full min-w-[50vw] bg-gray90 rounded-3xl overflow-hidden relative"
-                    ref={nftViewerRef}
-                >
-                    <div
-                        className="absolute top-3 right-3 bg-gray70 rounded-full p-4 
+        <Sheet isOpen={!!selectedMarker} onClose={onCloseViewer}>
+            <Sheet.Container style={{ backgroundColor: "none" }}>
+                {/* <Sheet.Header /> */}
+                <Sheet.Content>
+                    {!!selectedMarker && (
+                        <div
+                            className="h-full max-w-full min-w-[50vw] bg-gray90 rounded-3xl overflow-hidden relative"
+                            ref={nftViewerRef}
+                        >
+                            <div
+                                className="absolute top-3 right-3 bg-gray70 rounded-full p-4 
                                 cursor-pointer hover:opacity-50 duration-300"
-                        onClick={onCloseViewer}
-                    >
-                        <img src={closeIcon} />
-                    </div>
-                    <img
-                        src={`${selectedMarker.previewUrl ?? selectedMarker.imageUrl}`}
-                        className="h-full w-full object-contain"
-                    />
-                    <div
-                        className="absolute left-0 bottom-0 right-0 p-4
+                                onClick={onCloseViewer}
+                            >
+                                <img src={closeIcon} />
+                            </div>
+                            <img
+                                src={`${selectedMarker.previewUrl ?? selectedMarker.imageUrl}`}
+                                className="h-full w-full object-contain"
+                            />
+                            <div
+                                className="absolute left-0 bottom-0 right-0 p-4
                                bg-nft-viewer-desc-bg
                                flex flex-col gap-2"
-                    >
-                        <div className="flex gap-2 items-end">
-                            <img src={locationIcon} />
-                            <span className="light-12 text-gray70">{`${selectedMarker.lat}, ${selectedMarker.lng}`}</span>
+                            >
+                                <div className="flex gap-2 items-end">
+                                    <img src={locationIcon} />
+                                    <span className="light-12 text-gray70">{`${selectedMarker.lat}, ${selectedMarker.lng}`}</span>
+                                </div>
+                                <Title title={selectedMarker.name} className="text-white" />
+                                <TextP className="text-white">{selectedMarker.description}</TextP>
+                            </div>
                         </div>
-                        <Title title={selectedMarker.name} className="text-white" />
-                        <TextP className="text-white">{selectedMarker.description}</TextP>
-                    </div>
-                </div>
-            )}
-        </div>
+                    )}
+                </Sheet.Content>
+            </Sheet.Container>
+            <Sheet.Backdrop />
+        </Sheet>
+        // <div
+        //     className={`fixed right-0 bottom-0 left-0 ${selectedMarker ? "h-dvh" : "h-0"} duration-1000
+        //                 flex justify-center items-center bg-white30
+        //                 ${selectedMarker ? "p-5 md:p-10 lg:p-20 pt-40 md:pt-10" : "p-0"} z-30`}
+        // >
+
+        // </div>
     );
 };
 
