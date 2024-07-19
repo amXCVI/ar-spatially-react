0e8b21156d9d1c95d9a3384a16f5afc18a070bf0
Andrey <am.xcvi@gmail.com>
2024-07-19 16:28:53 +0300

    fix get started section

--- src/pages/home-page/ui/sections/get-started-section.tsx
+++ src/pages/home-page/ui/sections/get-started-section.tsx
@@ -19,36 +19,12 @@ const GetStartedSection = () => {
                            grid grid-cols-12 gap-1
                           "
             >
-                {/* <div className="col-span-12 lg:col-span-7 xl:col-span-5 bg-light-bg p-4 py-10 md:p-10">
-                    <div className="flex flex-col h-full">
-                        <Title title="Developers" />
-                        <Subtitle subtitle="Coming Soon" className="mt-4 uppercase !text-blue-accent" />
-                        <TextP className="mt-4">
-                            We aim to create a comprehensive toolkit that allows both existing and emerging applications
-                            to connect and utilize the open features of other applications in the ecosystem, including
-                            blockchain functions.
-                        </TextP>
-                        <div className="m-auto">
-                            <DefaultButton className="bg-white text-gray90 mt-4">Playground</DefaultButton>
-                        </div>
-
-                        <ForUsersText className="hidden lg:flex xl:hidden" />
-                    </div>
-                </div> */}
-
-                {/* <div className="col-span-12 lg:col-span-5 xl:col-span-7 bg-light-bg p-4 py-10 md:p-10">
-                    <div className="flex flex-col xl:flex-row gap-10 md:gap-4">
-                        <img
-                            src={phoneForUsers}
-                            className="object-contain max-h-[50vh] lg:max-h-[40vh] max-w-[40vw] mx-auto"
-                        />
-
-                        <ForUsersText className="flex lg:hidden xl:flex" />
-                    </div>
-                </div> */}
-
                 <div className="col-span-12 bg-light-bg px-4 py-10 md:p-10">
                     <div className="flex flex-col-reverse xl:flex-row gap-6 xl:gap-4 mx-auto max-w-7xl">
+                        <a href={routes.product} className="xl:hidden mx-auto">
+                            <DefaultButton className="bg-white text-gray90">Product</DefaultButton>
+                        </a>
+
                         <div className="flex gap-4 mx-auto xl:hidden">
                             <img
                                 src={phone1}
@@ -89,12 +65,9 @@ const GetStartedSection = () => {
                                 </TextP>
                             </div>
 
-                            <div className="flex gap-2 md:gap-4">
-                                <a href={routes.product}>
+                            <div className="gap-2 md:gap-4 hidden xl:flex">
+                                <a href={routes.product} className="">
                                     <DefaultButton className="bg-white text-gray90">Product</DefaultButton>
-                                </a>
-                                <a href={routes.product}>
-                                    <DefaultButton className="bg-white text-gray90">Ar</DefaultButton>
                                 </a>
                             </div>
                         </div>
