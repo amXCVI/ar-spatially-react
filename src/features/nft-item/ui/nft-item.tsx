601596821bc1f8c84691eda4a497680310104d53
Andrey <am.xcvi@gmail.com>
2024-07-19 12:15:41 +0300

    add bottomscheet for nft viewer

--- src/features/nft-item/ui/nft-item.tsx
+++ src/features/nft-item/ui/nft-item.tsx
@@ -16,44 +16,42 @@ const NftItem = ({ selectedMarker, onCloseViewer }: NftItemInterface) => {
         onCloseViewer();
     });
 
-    if (!selectedMarker) {
-        return <div />;
-    }
-
     return (
         <div
-            className="fixed top-0 right-0 bottom-0 left-0 bg-white30
-                           flex justify-center items-center
-                           p-5 md:p-10 lg:p-20 pt-40 md:pt-10"
+            className={`fixed right-0 bottom-0 left-0 ${selectedMarker ? "h-dvh" : "h-0"} duration-1000
+                        flex justify-center items-center bg-white30
+                        ${selectedMarker ? "p-5 md:p-10 lg:p-20 pt-40 md:pt-10" : "p-0"} z-30`}
         >
-            <div
-                className="h-full max-w-full min-w-[50vw] bg-gray90 rounded-3xl overflow-hidden relative"
-                ref={nftViewerRef}
-            >
+            {!!selectedMarker && (
                 <div
-                    className="absolute top-3 right-3 bg-gray70 rounded-full p-4 
-                                cursor-pointer hover:opacity-50 duration-300"
-                    onClick={onCloseViewer}
+                    className="h-full max-w-full min-w-[50vw] bg-gray90 rounded-3xl overflow-hidden relative"
+                    ref={nftViewerRef}
                 >
-                    <img src={closeIcon} />
-                </div>
-                <img
-                    src={`${selectedMarker.previewUrl ?? selectedMarker.imageUrl}`}
-                    className="h-full w-full object-contain"
-                />
-                <div
-                    className="absolute left-0 bottom-0 right-0 p-4
+                    <div
+                        className="absolute top-3 right-3 bg-gray70 rounded-full p-4 
+                                cursor-pointer hover:opacity-50 duration-300"
+                        onClick={onCloseViewer}
+                    >
+                        <img src={closeIcon} />
+                    </div>
+                    <img
+                        src={`${selectedMarker.previewUrl ?? selectedMarker.imageUrl}`}
+                        className="h-full w-full object-contain"
+                    />
+                    <div
+                        className="absolute left-0 bottom-0 right-0 p-4
                                bg-nft-viewer-desc-bg
                                flex flex-col gap-2"
-                >
-                    <div className="flex gap-2 items-end">
-                        <img src={locationIcon} />
-                        <span className="light-12 text-gray70">{`${selectedMarker.lat}, ${selectedMarker.lng}`}</span>
+                    >
+                        <div className="flex gap-2 items-end">
+                            <img src={locationIcon} />
+                            <span className="light-12 text-gray70">{`${selectedMarker.lat}, ${selectedMarker.lng}`}</span>
+                        </div>
+                        <Title title={selectedMarker.name} className="text-white" />
+                        <TextP className="text-white">{selectedMarker.description}</TextP>
                     </div>
-                    <Title title={selectedMarker.name} className="text-white" />
-                    <TextP className="text-white">{selectedMarker.description}</TextP>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
