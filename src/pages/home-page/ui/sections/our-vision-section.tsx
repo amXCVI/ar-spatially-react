0612566648a20a8bc6b5f4881f4423ef88f270e1
Andrey <am.xcvi@gmail.com>
2024-08-16 13:29:57 +0300

    fix our vision section

--- src/pages/home-page/ui/sections/our-vision-section.tsx
+++ src/pages/home-page/ui/sections/our-vision-section.tsx
@@ -1,13 +1,12 @@
-// import { OrbitControls, useGLTF } from "@react-three/drei";
 import Spline from "@splinetool/react-spline";
 
-// import { Suspense } from "react";
-// import { Canvas, useFrame } from "react-three-fiber";
 import { DefaultButton } from "@/shared/ui/buttons";
 
 import appStoreButton from "/images/landing/get-started-section/app-store-button.svg";
 import playMarketButton from "/images/landing/get-started-section/play-market-button.svg";
 import textParagraphPoint from "/images/landing/our-vision-section/paragraph-point.svg";
+import topText from "/images/landing/our-vision-section/top-text.svg";
+import textItemPointIcon from "/images/landing/start-section/text-item-point.svg";
 
 const OurVisionSection = () => {
     return (
@@ -19,14 +18,14 @@ const OurVisionSection = () => {
             //     flex flex-col justify-between
             //     px-4 mx-auto container"
             className="
-                xl:bg-[url(/images/landing/our-vision-section/bg-image.png)]
+                xl:bg-[url(/images/landing/our-vision-section/bg-image.webp)]
                 bg-[length:auto_150vh]
                 bg-no-repeat bg-top
                 h-full xl:min-h-[80dvh]
                 flex flex-col justify-between
                 xl:-mt-6
                 p-4
-                lg:rounded-b-[80px] overflow-hidden
+                
                 "
         >
             {/* <div className="hidden xl:flex relative mx-auto justify-between items-end">
@@ -46,74 +45,85 @@ const OurVisionSection = () => {
             <div
                 className="container mx-auto 
                         flex flex-col items-center 
-                        xl:grid xl:grid-cols-6
-                        gap-6 md:p-y-24
-                        xl:pl-16 2xl:pl-28"
+                        
+                        bg-[url(/images/landing/our-vision-section/glass-effect.webp)]
+                        bg-no-repeat bg-contain bg-center
+                        pb-20
+                        -mb-10
+                        "
             >
-                <div
-                    className="md:col-span-3 flex flex-col justify-center gap-4
-                               text-center max-w-prose md:max-w-full"
-                >
-                    <div className="flex gap-6 justify-center flex-wrap">
-                        <h1 className="h1-90-600 bg-our-vision-secton-text-gradient bg-clip-text text-transparent">
-                            OUR
-                        </h1>
-                        <h1 className="h1-90-600 bg-our-vision-secton-text-gradient bg-clip-text text-transparent">
-                            Vision
-                        </h1>
-                    </div>
+                <img src={topText} className="hidden lg:block mr-auto" />
 
-                    <Paragraph
-                        title="We are bringing together AR application developers and AR artists"
-                        text="to create a unified AR space that provides a new method of communication between physical businesses and users."
-                    />
-
-                    <Paragraph
-                        className="xl:ml-20"
-                        title="We link AR objects with the real world"
-                        text="using geolocation technologies and apply blockchain technology to secure this connection and ensure fair and transparent circulation of AR content. "
-                    />
-
-                    <Paragraph
-                        title="We aim to make AR and blockchain technologies a part of everyday life."
-                        text="We are creating a unique ecosystem that unites all applications, making it accessible for both users and businesses through our app. Our Playground ensures seamless integration, simplifying the process for everyone involved."
-                    />
-
-                    <div className="flex gap-2 md:gap-5 mx-auto mt-6">
-                        <a href={import.meta.env.VITE_APP_NFTST_APPSTORE_URL}>
-                            <DefaultButton className="bg-none !py-3">
-                                <img src={appStoreButton} />
-                            </DefaultButton>
-                        </a>
-                        <a href={import.meta.env.VITE_APP_NFTST_PLAYMARKET_URL}>
-                            <DefaultButton className="bg-none !py-3">
-                                <img src={playMarketButton} />
-                            </DefaultButton>
-                        </a>
-                    </div>
-                </div>
-
-                <div className="col-span-1 md:col-span-3 flex flex-col items-center w-full h-min">
+                <div className="flex flex-col lg:flex-row w-full items-center">
                     <div
-                        className="relative flex justify-center w-full h-min
-                                    scale-75 md:scale-90 lg:scale-100 xl:scale-110"
+                        className="flex flex-col justify-center
+                                   w-full
+                                   pt-6 gap-10 lg:gap-20
+                                   lg:max-w-prose lg:mr-auto"
                     >
-                        <Spline
-                            scene="https://prod.spline.design/oMmUby6YEqG3VTjS/scene.splinecode"
-                            className="!h-[400px] !xl:h-[600px] !w-[600px]"
-                            // className="object-contain h-full max-h-[80dvh] lg:max-h-[50dvh]"
-                        />
-                        {/* <img src={bulb} className="object-contain h-full max-h-[80dvh] lg:max-h-[50dvh]" /> */}
-                        {/* <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center">
+                        <div className="flex flex-col lg:flex-row gap-6 justify-center items-center flex-wrap mr-auto ml-auto lg:ml-0 lg:flex-nowrap">
+                            <h1 className="h1-90-600 bg-our-vision-secton-text-gradient bg-clip-text text-transparent">
+                                OUR
+                            </h1>
+                            <h1 className="h1-90-600 bg-our-vision-secton-text-gradient bg-clip-text text-transparent">
+                                Vision
+                            </h1>
+                        </div>
+
+                        <div className="flex flex-col gap-6 items-start">
+                            <Paragraph
+                                title="We are bringing together AR application developers and AR artists"
+                                text="to create a unified AR space that provides a new method of communication between physical businesses and users."
+                            />
+
+                            <Paragraph
+                                className="xl:ml-20"
+                                title="We link AR objects with the real world"
+                                text="using geolocation technologies and apply blockchain technology to secure this connection and ensure fair and transparent circulation of AR content. "
+                            />
+
+                            <Paragraph
+                                title="We aim to make AR and blockchain technologies a part of everyday life."
+                                text="We are creating a unique ecosystem that unites all applications, making it accessible for both users and businesses through our app. Our Playground ensures seamless integration, simplifying the process for everyone involved."
+                            />
+                        </div>
+
+                        <div className="flex gap-2 md:gap-5 mx-auto mt-6">
+                            <a href={import.meta.env.VITE_APP_NFTST_APPSTORE_URL}>
+                                <DefaultButton className="bg-none !py-3">
+                                    <img src={appStoreButton} />
+                                </DefaultButton>
+                            </a>
+                            <a href={import.meta.env.VITE_APP_NFTST_PLAYMARKET_URL}>
+                                <DefaultButton className="bg-none !py-3">
+                                    <img src={playMarketButton} />
+                                </DefaultButton>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="col-span-1 md:col-span-3 flex flex-col items-center w-full h-min">
+                        <div
+                            className="relative flex justify-center w-full h-min
+                                    scale-75 md:scale-90 lg:scale-125 xl:scale-150"
+                        >
+                            <Spline
+                                scene="https://prod.spline.design/oMmUby6YEqG3VTjS/scene.splinecode"
+                                className="!h-[400px] !xl:h-[600px] !w-[600px]"
+                                // className="object-contain h-full max-h-[80dvh] lg:max-h-[50dvh]"
+                            />
+                            {/* <img src={bulb} className="object-contain h-full max-h-[80dvh] lg:max-h-[50dvh]" /> */}
+                            {/* <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center">
                             <img src={logo} className="" />
                         </div> */}
-                        {/* <Canvas>
+                            {/* <Canvas>
                                 <ambientLight intensity={1} />
                                 <Suspense fallback={null}>
                                     <Model />
                                 </Suspense>
                                 <OrbitControls />
                             </Canvas> */}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -130,10 +140,14 @@ const OurVisionSection = () => {
 
 const Paragraph = ({ title, text, className }: { title: string; text: string; className?: string }) => {
     return (
-        <div className={`flex items-start gap-2 xl:ml-auto justify-center text-gray70 ${className}`}>
+        <div className={`flex items-start gap-2 justify-center text-gray70 ${className}`}>
             <div className="flex flex-col gap-2">
-                <div className="flex flex-col manrope-semibold-22 text-black mt-2">{title}</div>
-                <div className="flex gap-2 items-start xl:ml-10">
+                <div className="flex items-center">
+                    <img src={textItemPointIcon} className="mr-2" />
+                    <div className="flex flex-col manrope-semibold-22 text-black mt-2">{title}</div>
+                </div>
+
+                <div className="flex gap-2 items-start ml-10 lg:ml-16">
                     <img src={textParagraphPoint} />
                     <span className="manrope-regular-16">{text}</span>
                 </div>
--- src/pages/home-page/ui/sections/start-section.tsx
+++ src/pages/home-page/ui/sections/start-section.tsx
@@ -37,20 +37,20 @@ const StartSection = () => {
                 <div className="container mx-auto flex flex-col items-center lg:gap-8 h-full lg:max-h-full lg:flex-row pb-10 lg:py-10">
                     <div className="flex flex-col justify-between w-full h-full gap-10 md:gap-20">
                         <div
-                            className="flex flex-wrap flex-col w-full px-20
+                            className="flex flex-wrap flex-col w-full md:px-20
                                         h1-90-600"
                         >
                             <h1 className="bg-start-section-text-gradient bg-clip-text text-transparent md:mr-auto">
                                 OUR
                             </h1>
-                            <h1 className="bg-start-section-text-gradient bg-clip-text text-transparent ml-auto">
+                            <h1 className="bg-start-section-text-gradient bg-clip-text text-transparent md:ml-auto">
                                 Solution
                             </h1>
                         </div>
 
                         <div className="flex flex-col w-full gap-6 items-start">
                             <div className="flex justify-start items-center manrope-regular-26 text-gray70">
-                                <img src={textItemPointIcon} className="hidden md:block mr-2" />
+                                <img src={textItemPointIcon} className="mr-2" />
                                 <div className="">
                                     <b className="text-gray90">Aggregator of AR apps </b>
                                     <br className="hidden sm:block" />
@@ -59,7 +59,7 @@ const StartSection = () => {
                             </div>
 
                             <div className="flex justify-start items-center  manrope-regular-26 text-gray70 lg:ml-24 2xl:ml-40">
-                                <img src={textItemPointIcon} className="hidden md:block mr-2" />
+                                <img src={textItemPointIcon} className="mr-2" />
                                 <div className="">
                                     <b className="text-gray90">AR content </b>tied to location
                                     <br className="hidden sm:block" />
@@ -68,12 +68,12 @@ const StartSection = () => {
                             </div>
 
                             <div className="flex justify-start items-center  manrope-regular-26 text-gray70 lg:ml-10 2xl:ml-16">
-                                <img src={textItemPointIcon} className="hidden md:block mr-2" />
+                                <img src={textItemPointIcon} className="mr-2" />
                                 <div className="">
                                     <b className="text-gray90">AR-NFT</b> is a spatial geolocation token{" "}
                                     <br className="hidden sm:block" />
                                     that provide rights for
-                                    <span className="md:text-gray90 ml-2">AR content</span>
+                                    <span className="text-gray90 ml-2">AR content</span>
                                 </div>
                             </div>
                         </div>
