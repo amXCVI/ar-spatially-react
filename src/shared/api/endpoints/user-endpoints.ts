dec00fec27040dbc555ef50001bc856ab86ffcd2
Andrey <am.xcvi@gmail.com>
2024-09-02 17:34:03 +0300

    fix google auth

--- src/shared/api/endpoints/user-endpoints.ts
+++ src/shared/api/endpoints/user-endpoints.ts
@@ -1,4 +1,4 @@
-import { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 
 import { ApiConstants } from "@/shared/config";
 import { ApiResponseInterface, UserInterface } from "@/shared/types";
@@ -53,19 +53,6 @@ const getMe = async () => {
     }
 };
 
-const getAccessTokenFromX = async ({ xCode }: { xCode: string }) => {
-    const url = `https://api.x.com/2/oauth2/token?code=${xCode}&grant_type=authorization_code&client_id=${import.meta.env.VITE_APP_X_CLIENT_ID}&redirect_uri=https://arspatially.com&code_verifier=challenge`;
-
-    try {
-        const response: AxiosResponse<ApiResponseInterface<{ token: string; user: UserInterface }>> =
-            await apiClient.post(url);
-
-        return response.data.data;
-    } catch (error) {
-        throw new Error(`${url} ErrorRequest: ${error}`);
-    }
-};
-
 const signupX = async ({ twitterCode }: { twitterCode: string }) => {
     const url = `/gateway/user/signup-x`;
 
@@ -87,4 +74,28 @@ const signupX = async ({ twitterCode }: { twitterCode: string }) => {
     }
 };
 
-export const userApi = { login, signup, getMe, signupX, getAccessTokenFromX };
+const signupGoogle = async ({ googleToken }: { googleToken: string }) => {
+    const url = `/gateway/user/signup-google`;
+
+    try {
+        const response: AxiosResponse<ApiResponseInterface<{ token: string; user: UserInterface }>> =
+            await apiClient.post(url, {
+                system: ApiConstants.system,
+                token: googleToken,
+            });
+
+        return response.data.data;
+    } catch (error) {
+        throw new Error(`${url} ErrorRequest: ${error}`);
+    }
+};
+
+const getGoogleUserInfo = async ({ access_token }: { access_token: string }) => {
+    await axios
+        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+            headers: { Authorization: `Bearer ${access_token}` },
+        })
+        .then((res) => res.data);
+};
+
+export const userApi = { login, signup, getMe, signupX, signupGoogle, getGoogleUserInfo };
