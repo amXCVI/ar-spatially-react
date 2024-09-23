2a18c3745fb1311880446f69d20fd8f725d3ff93
Andrey <am.xcvi@gmail.com>
2024-09-23 12:28:54 +0300

    fix profile

--- src/entities/profile-info/model/use-profile-hook.ts
+++ src/entities/profile-info/model/use-profile-hook.ts
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 import { useUserContext } from "@/shared/stores";
 
 const useProfileHook = () => {
+    const [copyedNickname, setCopyedNickname] = useState<boolean>(false);
+
     const { user } = useUserContext();
 
-    return { user };
+    const copyToClipboardNickname = () => {
+        if (user) {
+            navigator.clipboard.writeText(user.nickname);
+            setCopyedNickname(true);
+            setTimeout(() => {
+                setCopyedNickname(false);
+            }, 1000);
+        }
+    };
+
+    return { user, copyToClipboardNickname, copyedNickname };
 };
 
 export { useProfileHook };
--- src/entities/profile-info/ui/profile-info.tsx
+++ src/entities/profile-info/ui/profile-info.tsx
@@ -4,7 +4,7 @@ import DefaultUserAvatar from "../assets/default-avatar.svg?react";
 import { useProfileHook } from "../model";
 
 const ProfileInfo = ({ className }: { className?: string }) => {
-    const { user } = useProfileHook();
+    const { user, copyToClipboardNickname } = useProfileHook();
 
     return (
         <div
@@ -26,18 +26,24 @@ const ProfileInfo = ({ className }: { className?: string }) => {
                 )}
             </div>
             {!!user && (
-                <div className="flex flex-col gap-3">
-                    <h3 className="onest-bold-24 text-white">{user.name ?? user.nickname}</h3>
-                    <div className="flex gap-3 items-center flex-wrap">
+                <div className="flex flex-col gap-3 max-w-full overflow-hidden">
+                    <h3 className="onest-bold-24 text-white text-wrap text-ellipsis overflow-hidden">
+                        {user.name ?? user.nickname}
+                    </h3>
+                    <div className="flex gap-3 items-center flex-wrap w-full">
                         <div
                             className="flex gap-2.5 items-center px-2 py-1
                             manrope-regular-16 text-white
-                            bg-dark-charcoal rounded-lg"
+                            bg-dark-charcoal rounded-lg
+                            cursor-pointer"
+                            onClick={copyToClipboardNickname}
                         >
                             <span>@{user.nickname}</span>
                             <CopyIcon />
                         </div>
-                        <span className="manrope-regular-16 text-american-silver">{user.email}</span>
+                        <span className="manrope-regular-16 text-american-silver max-w-full text-ellipsis overflow-hidden">
+                            {user.email}
+                        </span>
                     </div>
                 </div>
             )}
