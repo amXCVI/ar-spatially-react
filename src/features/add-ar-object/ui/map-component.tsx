1ffa6c7190e02540f04cd6f589f6a2572143da80
Andrey <am.xcvi@gmail.com>
2024-10-23 16:11:41 +0300

    add other polygons on map

--- src/features/add-ar-object/ui/map-component.tsx
+++ src/features/add-ar-object/ui/map-component.tsx
@@ -1,83 +1,126 @@
 import { DefaultMap } from "@ar-kit/lib/map-component";
-import { MapMouseEvent, useMap, useMapsLibrary } from "@vis.gl/react-google-maps";
-import { useEffect } from "react";
+import { AdvancedMarker, MapMouseEvent, useMap } from "@vis.gl/react-google-maps";
+import { useEffect, useRef, useState } from "react";
 
 import "./index.css";
 
-const polygonCoords = [
-    { lat: 25.105748, lng: 55.161477 },
-    { lat: 25.122003, lng: 55.1783 },
-    { lat: 25.132473, lng: 55.187913 },
-    { lat: 25.13763, lng: 55.188256 },
-    { lat: 25.146849, lng: 55.195981 },
-    { lat: 25.134817, lng: 55.214177 },
-    { lat: 25.12966, lng: 55.210915 },
-    { lat: 25.123722, lng: 55.202504 },
-    { lat: 25.111219, lng: 55.219498 },
-    { lat: 25.101997, lng: 55.212804 },
-    { lat: 25.095745, lng: 55.221043 },
-    { lat: 25.080894, lng: 55.207825 },
-    { lat: 25.081206, lng: 55.205422 },
-    { lat: 25.085115, lng: 55.196839 },
-    { lat: 25.077767, lng: 55.200787 },
-    { lat: 25.06823, lng: 55.192719 },
-    { lat: 25.062132, lng: 55.185853 },
-    { lat: 25.057158, lng: 55.175553 },
-    { lat: 25.052311, lng: 55.169717 },
-    { lat: 25.042615, lng: 55.16079 },
-    { lat: 25.053562, lng: 55.145684 },
-    { lat: 25.059347, lng: 55.141221 },
-    { lat: 25.06138, lng: 55.137787 },
-    { lat: 25.069198, lng: 55.136243 },
-    { lat: 25.084348, lng: 55.151435 },
-    { lat: 25.089108, lng: 55.146671 },
-    { lat: 25.105748, lng: 55.161477 },
-];
+import { worlds } from "../model/data";
 
 const MapComponent = () => {
-    const map = useMap();
+    const polygonsRef = useRef<google.maps.Polygon[]>([]);
 
-    // const geometryLibrary = useMapsLibrary("geometry");
+    const [cursorPosition, setCursorPosition] = useState<{ lat: number; lng: number } | null>(null);
+    const [savedPosition, setSavedPosition] = useState<{ lat: number; lng: number } | null>(null);
+
+    const handleMouseMove = (event: MapMouseEvent) => {
+        if (!savedPosition) {
+            setCursorPosition(event.detail.latLng);
+        }
+    };
+    const [mapZoom, setMapZoom] = useState<number>(11);
+    const map = useMap();
 
     useEffect(() => {
         if (map) {
-            const polygon = new google.maps.Polygon({
-                paths: polygonCoords,
-                strokeColor: "#FF0000",
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: "#FF0000",
-                fillOpacity: 0.35,
-            });
+            worlds.forEach((world) => {
+                const polygon = new google.maps.Polygon({
+                    paths: world.polygon,
+                    strokeColor: world.worldPolygonColor,
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: world.worldPolygonColor,
+                    fillOpacity: 0.35,
+                });
 
-            polygon.setMap(map);
+                polygonsRef.current.push(polygon);
 
-            polygon.addListener("click", (e: { latLng: { lat: () => number; lng: () => number } }) => {
-                console.log({ lat: e.latLng.lat(), lng: e.latLng.lng() });
-                alert(
-                    `Coordinates clicked inside the polygon: ${JSON.stringify({ lat: e.latLng.lat(), lng: e.latLng.lng() })}`,
+                polygon.setMap(map);
+
+                polygon.addListener(
+                    "click",
+                    (e: { domEvent: MouseEvent; latLng: { lat: () => number; lng: () => number } }) => {
+                        setSavedPosition({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+                    },
                 );
+
+                polygon.addListener("mouseover", () => {
+                    polygon.setOptions({ fillColor: "#00FF00", strokeColor: "#00FF00" }); // Изменение цвета при наведении
+                    const marker = document.getElementById(`${world.worldId}Marker`);
+                    if (marker) {
+                        marker.classList.replace("opacity-50", "opacity-100");
+                        marker.classList.replace("scale-100", "scale-150");
+                    }
+                });
+
+                polygon.addListener("mouseout", () => {
+                    polygon.setOptions({ fillColor: world.worldPolygonColor, strokeColor: world.worldPolygonColor }); // Возврат к изначальному цвету
+                    const marker = document.getElementById(`${world.worldId}Marker`);
+                    if (marker) {
+                        marker.classList.replace("opacity-100", "opacity-50");
+                        marker.classList.replace("scale-150", "scale-100");
+                    }
+                });
+
+                polygon.addListener("mousemove", (e: { latLng: { lat: () => number; lng: () => number } }) => {
+                    if (!savedPosition) {
+                        setCursorPosition({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+                    }
+                });
             });
         }
-    }, [map]);
+
+        return () => {
+            // Удаляем полигоны при размонтировании
+            polygonsRef.current.forEach((polygon) => {
+                polygon.setMap(null);
+            });
+            polygonsRef.current = []; // очищаем массив
+        };
+    }, [map, savedPosition]);
+
+    // Размер картинки Мира зависит от масштаба карты
+    const markerSize = mapZoom * mapZoom;
 
     return (
         <DefaultMap
-            // onChangeMapCenter={onChangeMapCenter}
-            // onChangeCoords={onChangeCoords}
-
-            // ref={mapRef}
-
+            onMousemove={handleMouseMove}
             defaultCenter={{ lat: 25.1132415, lng: 55.2233329 }}
             mapId={import.meta.env.VITE_APP_GOOGLE_MAP_ID}
             className="g-map-polygons"
             disableDefaultUI={true}
             reuseMaps={true}
             minZoom={3}
-            defaultZoom={12}
+            defaultZoom={11}
+            onZoomChanged={(e) => setMapZoom(e.detail.zoom)}
             backgroundColor={"black"}
             gestureHandling={"greedy"}
-        ></DefaultMap>
+        >
+            {worlds.map((world) => {
+                return (
+                    <AdvancedMarker
+                        position={world.polygonCenterCoords}
+                        title={world.worldName}
+                        className={`aspect-square`}
+                        key={world.worldId + "WorldMarker"}
+                        style={{ width: `${markerSize}px` }}
+                    >
+                        <img
+                            src={world.worldImgSrc}
+                            className="w-full h-full duration-500 opacity-50 scale-100 z-0"
+                            id={`${world.worldId}Marker`}
+                        />
+                    </AdvancedMarker>
+                );
+            })}
+
+            <AdvancedMarker position={savedPosition ?? cursorPosition}>
+                <img
+                    src="/event-page/butterflyes/ice/klll_3d_high_poly_textured_model_of_a_butterfly_body_legs_and_w_b29759ff-fdf9-415a-8a6e-5464b6c32519 1.webp"
+                    alt="Cursor"
+                    className="aspect-square rounded-xl w-24 z-50"
+                />
+            </AdvancedMarker>
+        </DefaultMap>
     );
 };
 
