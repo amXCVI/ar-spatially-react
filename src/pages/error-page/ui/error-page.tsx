2abda28b51e968fc6c94348174d6dad2f3dd57b2
Andrey <am.xcvi@gmail.com>
2024-11-15 11:43:32 +0300

    add error page

--- src/pages/error-page/ui/error-page.tsx
+++ src/pages/error-page/ui/error-page.tsx
@@ -1,5 +1,16 @@
+import { routes } from "@/shared/config";
+
 const ErrorPage = () => {
-    return <div>Error page</div>;
+    return (
+        <div className="bg-dark-gray w-full h-full min-h-dvh">
+            <div className="flex flex-col container p-6 mx-auto">
+                <h1 className="text-white mx-auto">Error</h1>
+                <a href={`/${routes.home}`} className="text-white">
+                    Home page
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default ErrorPage;
